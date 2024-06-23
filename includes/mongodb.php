8ab11b86e949bdcaf47be4b640cdf51fb7659d32
Akilanpalani <akilanpalani2001@gmail.com>
2024-06-23 20:01:11 +0530

    feat: :zap: Added mysql test connection for heroku production and update the env to getenv for heroku production

--- includes/mongodb.php
+++ includes/mongodb.php
@@ -5,7 +5,7 @@ require __DIR__ . '/../vendor/autoload.php';
 
 use MongoDB\Client as MongoDB;
 
-$mongodb_uri = $_ENV['MONGO_URI'];
+$mongodb_uri = getenv('MONGO_URI');
 
 $client = new MongoDB($mongodb_uri);
 
